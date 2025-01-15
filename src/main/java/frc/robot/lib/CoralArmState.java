bcf35e053ffa3e2c8e0a2f7812e0cfd2cf49073f
Darryl <darryl.greathouse@gmail.com>
2025-01-15 14:38:54 -0500

    Updated states

---
+++ src/main/java/frc/robot/lib/CoralArmState.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.lib;
+
+/** Add your docs here. */
+public enum CoralArmState {
+    L4,
+    L3,
+    L2,
+    L1,
+    FLOOR_VERT,
+    FLOOR_HORZ,
+    START,
+}
