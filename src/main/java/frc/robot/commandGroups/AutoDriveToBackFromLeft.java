3e4cfadc2eaa359bb9f2ae03ee988307babe62ee
Darryl <darryl.greathouse@gmail.com>
2025-01-26 08:52:16 -0500

    Code cleanup

--- src/main/java/frc/robot/commandGroups/AutoDriveToBackFromLeft.java
+++ src/main/java/frc/robot/commandGroups/AutoDriveToBackFromLeft.java
@@ -6,11 +6,9 @@ package frc.robot.commandGroups;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.drive.AutoDriveToPose;
-import frc.robot.commands.drive.AutoRotateToAngle;
 import frc.robot.lib.StartLocation;
 import frc.robot.lib.g;
 
